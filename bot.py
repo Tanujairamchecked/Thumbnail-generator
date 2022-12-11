6c9fd0502ebfec07560e29bb4d9453aece9fbc6a
Tanujairamchecked <94793933+Tanujairamchecked@users.noreply.github.com>
2022-12-11 15:08:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ from pytube.exceptions import VideoUnavailable
 import os 
 
 TOKEN = os.environ.get("TOKEN","")
-API_ID =int(os.environ.get("API_ID",""))
+API_ID =os.environ.get("API_ID","")
 API_HASH =os.environ.get("API_HASH","")
 
 
